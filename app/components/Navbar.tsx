841895b89583d3379fd34a3ed5abf11ad918fe69
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-03-26 00:07:57 +0100

    second commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className=""></nav>
+    </div>
+  );
+};
+
+export default Navbar;
