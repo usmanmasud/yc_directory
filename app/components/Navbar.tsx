bd018d7b1efaebadbcad55913195f814af744171
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-03-26 15:47:14 +0100

    update navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,44 @@
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav className=""></nav>
-    </div>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span>Logout</span>
+              </button>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
