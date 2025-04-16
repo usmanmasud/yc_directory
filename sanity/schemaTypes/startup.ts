23d14195c807feab9df17072511f7253a946f113
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-16 23:57:55 +0100

    add startups schema

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,55 @@
+import { UserIcon } from "lucide-react";
+import { title } from "process";
+import { defineField, defineType } from "sanity";
+
+export const startup = defineType({
+  name: "startup",
+  title: "Startup",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "views",
+      type: "number",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+    }),
+    defineField({
+      name: "category",
+      type: "string",
+      validation: (Rule) =>
+        Rule.min(1).max(20).required().error("Category is required"),
+    }),
+    defineField({
+      name: "image",
+      type: "url",
+      validation: (Rule) => Rule.required().error("Image is required"),
+    }),
+    defineField({
+      name: "pitch",
+      type: "text",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
