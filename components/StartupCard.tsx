66da32b20374a8fe54f706eea965a3d805212dd6
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-16 13:52:02 +0100

    finished implementing startup cards

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,24 +1,60 @@
 import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const StartupCard = ({ post }: { post: startupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author: { _id: authorId, name },
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
   return (
     <li className="startup-card group">
       <div className="flex-between">
-        <p className="startup_card_date">{formatDate(post._createdAt)}</p>
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
         <div className="flex gap-1.5">
           <EyeIcon className="size-6 text-primary" />
-          <span className="text-16-medium">{post.views}</span>
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${post.author?._d}`}>
-            <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`'/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src="https://placehold.co/48x48"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className=" rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`'/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <img src={image} alt="placeholder" className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
       </div>
     </li>
   );
